3ce607974418c8de7c7db0359e74afaf6fa8cb92
open-leocat <68399149+open-leocat@users.noreply.github.com>
2021-02-02 09:41:32 +0100

    Update Writer.java

--- src/net/davinci/converter/Writer.java
+++ src/net/davinci/converter/Writer.java
@@ -23,28 +23,28 @@ public class Writer {
         return buffer.toString();
     }
 
-	public static String writeDithPBM(BufferedImage image) {
-		BufferedImage dithImage = DitheringUtils.dithering(image);
+    public static String writeDithPBM(BufferedImage image) {
+	BufferedImage dithImage = DitheringUtils.dithering(image);
 		
-		final StringJoiner buffer = new StringJoiner(" ");
-		buffer.add("P1");
-		buffer.add(Integer.toString(dithImage.getWidth()));
-		buffer.add(Integer.toString(dithImage.getHeight()));
-        for (int y = 0; y < dithImage.getHeight(); ++y) {
-            for (int x = 0; x < dithImage.getWidth(); ++x) {
-                final int clr = dithImage.getRGB(x, y);
-                final int r = (clr & 0xFF0000) >> 16;
-                final int g = (clr & 0xFF00) >> 8;
-                final int b = clr & 0xFF;
-                
-                if(765==(r+g+b)) {
-                	buffer.add(Integer.toString(0));
-                } else {
-                	buffer.add(Integer.toString(1));
-                }
-            }
-        }
-		
-		return buffer.toString();
+	final StringJoiner buffer = new StringJoiner(" ");
+	buffer.add("P1");
+	buffer.add(Integer.toString(dithImage.getWidth()));
+	buffer.add(Integer.toString(dithImage.getHeight()));
+	    
+	for (int y = 0; y < dithImage.getHeight(); ++y) {
+		for (int x = 0; x < dithImage.getWidth(); ++x) {
+			final int clr = dithImage.getRGB(x, y);
+			final int r = (clr & 0xFF0000) >> 16;
+			final int g = (clr & 0xFF00) >> 8;
+                	final int b = clr & 0xFF;
+                	
+			if(765==(r+g+b)) {
+                		buffer.add(Integer.toString(0));
+                	} else {
+                		buffer.add(Integer.toString(1));
+                	}
+		}
 	}
-}
+		
+	return buffer.toString();
+    }
